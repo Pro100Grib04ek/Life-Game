b97d0eec55dfeff86118c7577aa1bf967bb03162
Pro100Grib04ek <mamnevilia@mail.ru>
2025-04-07 02:53:31 +0300

    Update main.c
    Доделал движок, теперь всё работает так, как и должно

--- main.c
+++ main.c
@@ -5,16 +5,19 @@
 #include <inttypes.h>
 #pragma warning(disable : 4996)
 
-#define SIZE 50
+#define SIZE 50 // Размер поля
+
+// Считает сумму вокруг клетки i, j в матрице
 #define Count_around(i, j){\
 copy_field[i-1][j-1]+copy_field[i-1][j]+copy_field[i-1][j+1]+\
 copy_field[i][j-1] +copy_field[i][j+1]+\
 copy_field[i+1][j-1]+copy_field[i+1][j]+copy_field[i+1][j+1] \
-}
-void upgrade_field(uint8_t(*field)[SIZE])
+} 
+
+void upgrade_field(uint8_t(*field)[SIZE]) 
 {
 	uint8_t copy_field[SIZE][SIZE];
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE; i++) // Создаём копию поля
 	{
 		for (int j = 0; j < SIZE; j++)
 		{
@@ -22,8 +25,141 @@ void upgrade_field(uint8_t(*field)[SIZE])
 		}
 	}
 
-	for (int i = 1; i < SIZE - 1; i++)
+	for (int i = 1; i < SIZE - 1; i++) 
 	{
+		uint16_t count; // Сумма вокруг клетки
+
+		// Углы
+		{
+			// Левый верхний
+			count = copy_field[0][1] + copy_field[1][0] + copy_field[1][1];
+			if (field[0][0] == 0 && count == 3)
+			{
+				field[0][0] = 1;
+			}
+			else if (field[0][0] == 1 && (count == 2 || count == 3))
+			{
+				field[0][0] = 1;
+			}
+			else
+			{
+				field[0][0] = 0;
+			}
+
+			// Правый верхний
+			count = copy_field[0][SIZE - 2] + copy_field[1][SIZE - 1] + copy_field[1][SIZE - 2];
+			if (field[0][SIZE - 1] == 0 && count == 3)
+			{
+				field[0][SIZE - 1] = 1;
+			}
+			else if (field[0][SIZE - 1] == 1 && (count == 2 || count == 3))
+			{
+				field[0][SIZE - 1] = 1;
+			}
+			else
+			{
+				field[0][SIZE - 1] = 0;
+			}
+
+			// Левый нижний
+			count = copy_field[SIZE - 2][0] + copy_field[SIZE - 1][1] + copy_field[SIZE - 2][1];
+			if (field[SIZE - 1][0] == 0 && count == 3)
+			{
+				field[SIZE - 1][0] = 1;
+			}
+			else if (field[SIZE - 1][0] == 1 && (count == 2 || count == 3))
+			{
+				field[SIZE - 1][0] = 1;
+			}
+			else
+			{
+				field[SIZE - 1][0] = 0;
+			}
+
+			// Правый нижний
+			count = copy_field[SIZE-1][SIZE-2] + copy_field[SIZE-2][SIZE-1] + copy_field[SIZE-2][SIZE-2];
+			if (field[SIZE - 1][SIZE - 1] == 0 && count == 3)
+			{
+				field[SIZE - 1][SIZE - 1] = 1;
+			}
+			else if (field[SIZE - 1][SIZE - 1] == 1 && (count == 2 || count == 3))
+			{
+				field[SIZE - 1][SIZE - 1] = 1;
+			}
+			else
+			{
+				field[SIZE - 1][SIZE - 1] = 0;
+			}
+		}
+		// Границы
+		{
+			// Верхняя 
+			{
+				count = copy_field[0][i - 1] + copy_field[0][i + 1] + copy_field[1][i - 1] + copy_field[1][i] + copy_field[1][i + 1];
+				if (field[0][i] == 0 && count == 3)
+				{
+					field[0][i] = 1;
+				}
+				if (field[0][i] == 1 && (count == 2 || count == 3))
+				{
+					field[0][i] = 1;
+				}
+				else
+				{
+					field[0][i] = 0;
+				}
+			}
+			// Правая 
+			{
+				count = copy_field[i - 1][SIZE - 1] + copy_field[i + 1][SIZE - 1] + copy_field[i - 1][SIZE - 2] + copy_field[i][SIZE - 2] + copy_field[i + 1][SIZE - 2];
+				if (field[i][SIZE - 1] == 0 && count == 3)
+				{
+					field[i][SIZE - 1] = 1;
+				}
+				if (field[i][SIZE - 1] == 1 && (count == 2 || count == 3))
+				{
+					field[i][SIZE - 1] = 1;
+				}
+				else
+				{
+					field[i][SIZE - 1] = 0;
+				}
+			}
+			// Нижняя 
+			{
+				count = copy_field[SIZE - 1][i - 1] + copy_field[SIZE - 1][i + 1] + copy_field[SIZE - 2][i - 1] + copy_field[SIZE - 2][i] + copy_field[SIZE - 2][i + 1];
+				if (field[SIZE - 1][i] == 0 && count == 3)
+				{
+					field[SIZE - 1][i] = 1;
+				}
+				if (field[SIZE - 1][i] == 1 && (count == 2 || count == 3))
+				{
+					field[SIZE - 1][i] = 1;
+				}
+				else
+				{
+					field[SIZE - 1][i] = 0;
+				}
+			}
+
+			// Левая 
+			{
+				count = copy_field[i - 1][0] + copy_field[i + 1][0] + copy_field[i - 1][1] + copy_field[i][1] + copy_field[i + 1][1];
+				if (field[i][0] == 0 && count == 3)
+				{
+					field[i][0] = 1;
+				}
+				if (field[i][0] == 1 && (count == 2 || count == 3))
+				{
+					field[i][0] = 1;
+				}
+				else
+				{
+					field[i][0] = 0;
+				}
+			}
+		}
+		// Внутренняя часть
 		for (int j = 1; j < SIZE - 1; j++)
 		{
 			uint16_t count = Count_around(i, j);
@@ -41,13 +177,15 @@ void upgrade_field(uint8_t(*field)[SIZE])
 			}
 		}
 	}
+	/* Тут было зануление границ, но оно как будто бы не нужно
 	for (int i = 0; i < SIZE; i++)
 	{
 		field[0][i] = 0;
 		field[SIZE-1][i] = 0;
-		field[i][0];
+		field[i][0] = 0;
 		field[i][SIZE - 1] = 0;
 	}
+	*/
 }
 
 void print_field(uint8_t(*field)[SIZE])
@@ -62,7 +200,8 @@ void print_field(uint8_t(*field)[SIZE])
 			}
 			else
 			{
-				printf("%"SCNu8, field[i][j]);
+				printf("o");
+				//printf("%"SCNu8, field[i][j]);
 			}
 		}
 		printf("\n");
@@ -88,7 +227,7 @@ int main()
 			char c;
 			fscanf(file_input, "%c", &c);
 			if (c == '0' || c == '.') { field[i][j] = 0; }
-			if (c == '1') { field[i][j] = 1; }
+			if (c == '1' || c == 'o') { field[i][j] = 1; }
 			if (c == '\n') { j--; }
 		}
 	}
@@ -96,7 +235,7 @@ int main()
 	while (1)
 	{
 		print_field(field);
-		Sleep(1000);
+		Sleep(650);
 		upgrade_field(*field);
 		system("cls");
 	}
