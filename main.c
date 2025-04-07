bb93d9c7630b75031fdf89df5c9356eb3b35a2d3
Pro100Grib04ek <mamnevilia@mail.ru>
2025-04-07 06:42:14 +0300

    Update main.c
    Сделал алгоритм хеширования поля и проверку на наличие циклов

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <windows.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <locale.h>
 #pragma warning(disable : 4996)
 
 #define SIZE 50 // Размер поля
@@ -13,6 +14,21 @@ copy_field[i-1][j-1]+copy_field[i-1][j]+copy_field[i-1][j+1]+\
 copy_field[i][j-1] +copy_field[i][j+1]+\
 copy_field[i+1][j-1]+copy_field[i+1][j]+copy_field[i+1][j+1] \
 } 
+
+// Функция хеширования djb2
+unsigned long djb2_hash(char* str) 
+{
+	unsigned long hash = 5381;  // Начальное значение
+	int c;
+
+	// Итеративно вычисляем хеш для каждого символа
+	while ((c = *str++)) 
+	{
+		hash = ((hash << 5) + hash) + c;  // hash * 33 + c
+	}
+
+	return hash;
+}
 
 void upgrade_field(uint8_t(*field)[SIZE]) 
 {
@@ -25,7 +41,7 @@ void upgrade_field(uint8_t(*field)[SIZE])
 		}
 	}
 
-	for (int i = 1; i < SIZE - 1; i++) 
+	for (int i = 1; i < SIZE - 1; i++) // Проход по внутренней части поля (Без углов)
 	{
 		uint16_t count; // Сумма вокруг клетки
 
@@ -210,6 +226,11 @@ void print_field(uint8_t(*field)[SIZE])
 
 int main()
 {
+	setlocale(LC_ALL, "rus");
+	printf("Enter the command: play | cycle\n");
+	char command[10];
+	scanf("%s", &command);
+
 	FILE* file_input = fopen("field.txt", "r");
 
 	uint8_t field[SIZE][SIZE];
@@ -231,7 +252,44 @@ int main()
 			if (c == '\n') { j--; }
 		}
 	}
+	fclose(file_input);
+	if (!strcmp(command, "cycle"))
+	{
+		char field_line[SIZE * SIZE + 1] = "";
+		unsigned long hash;
+		int ind = 0;
+		unsigned long* hashes = (unsigned long*)malloc(sizeof(unsigned long));
+		while (1)
+		{
+			int pos = 0;
+			for (int i = 0; i < SIZE; i++)
+			{
+				for (int j = 0; j < SIZE; j++)
+				{
+					field_line[pos++] = field[i][j] ? '1' : '0';
+				}
+			}
+			hash = djb2_hash(&field_line);
+			for (int i = 0; i < ind; i++)
+			{
+				if (hash == hashes[i])
+				{
+					printf("Цикл обнаружен!\n");
+					printf("Итерация: %d\n", ind + 1);
+					printf("Длина цикла: %d\n", ind - i);
+					Sleep(1500);
+					goto play;
+				}
+			}
+			hashes[ind] = hash;
+			ind++;
+			hashes = (unsigned long*)realloc(hashes, (ind + 1) * sizeof(unsigned long));
+			field_line[0] = '\0';
+			upgrade_field(*field);
+		}
+	}
 
+	play:
 	while (1)
 	{
 		print_field(field);
